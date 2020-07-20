2ce7477e3c6680f8acadde181df736b0dc104ef4
brianwlego <legobw@gmail.com>
2020-07-20 11:50:18 -0400

    fixed #books method in Author

--- author.rb
+++ author.rb
@@ -14,20 +14,16 @@ class Author
 
   def books
     Collaboration.all.select {|c| c.author == self}.map {|b| b.book}
-    
   end
   def write_book(title, word_count)
     Collaboration.new(self, Book.new(title, word_count))
   end
   def total_words
-    books.sum {|c| c.book.word_count}
+    books.sum {|b| b.word_count}
   end
   def self.most_words
     all.max {|a, b| a.total_words <=> b.total_words}
   end
-
-
-
 
 
 end
